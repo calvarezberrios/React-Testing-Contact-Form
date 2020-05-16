d3c5445779f6f1df11028dcfc8dfe27e73fd21e0
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-05-16 02:02:42 -0400

    added form tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
-import { act } from "react-dom/test-utils";
 
 test("renders App without crashing", () => {
   render(<App />);
